707d342019fa45b259a9ea3354e4b93c2249ae5d
corgiccori97 <tkdgmlgg7@gmail.com>
2023-03-22 18:11:10 +0900

    ing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,100 @@ import { useState } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 const Container = tw.div`
-  flex
-  items-center
-  justify-center
-  flex-col
-  w-full
-  bg-indigo-600
+  mt-16
+  px-4
 `;
 
+const H3 = tw.h3`
+  text-3xl
+  font-bold
+  text-center
+`;
+
+const Check = tw.div`
+flex
+flex-col
+items-center
+`;
+
+const H5 = tw.h5`
+text-sm
+text-gray-500
+font-medium
+`;
+
+const Method = tw.div`
+grid
+border-b
+w-full
+mt-8
+grid-cols-2
+`;
+
+const Button = tw.button`
+pb-4
+font-medium
+text-sm
+border-b-2
+border-transparent
+text-gray-500
+focus:border-orange-500
+focus:text-orange-400
+hover:text-gray-400
+`;
+
+const Form = tw.form`
+flex
+flex-col
+mt-8
+`;
+
+const Label = tw.label`
+text-sm
+font-medium
+text-gray-700
+`;
+
+const Input = tw.input`
+appearance-none
+w-full
+px-3
+py-2
+border
+border-gray-300
+rounded-md
+shadow-sm
+placeholder-gray-400
+focus:outline-none
+focus:ring-orange-500
+focus:border-orange-500
+required
+`;
+
+const Number = tw.div`
+flex
+rounded-md
+shadow-sm
+`;
+
+const LoginButton = tw.button`
+mt-5
+bg-orange-500
+hover:bg-orange-600
+text-white
+py-2
+px-4
+border
+border-transparent
+rounded-md
+shadow-sm
+text-sm
+font-medium
+focus:rng-2
+focus:ring-offset-2
+focus:ring-orange-500
+focus:outline-none
+`;
 export default function Home() {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
@@ -20,44 +106,49 @@ export default function Home() {
   return (
     <>
     <Container>
-      <h3>Enter to Carrot</h3>
-      <div>
-        <div>
-          <h5>Enter using:</h5>
-          <div>
-            <button onClick={onEmailClick}>Email</button>
-            <button onClick={onPhoneClick}>Phone</button>
-          </div>
-        </div>
-        <form>
-          <label>
+      <H3>Enter to Carrot</H3>
+      <div className='mt-12'>
+        <Check>
+          <H5>Enter using:</H5>
+          <Method>
+            <Button onClick={onEmailClick}>Email</Button>
+            <Button onClick={onPhoneClick}>Phone</Button>
+          </Method>
+        </Check>
+        <Form>
+          <Label>
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
-          </label>
+          </Label>
           <div>
-            {method === "email" ? <input type="email" required /> : null}
+            {method === "email" ?
+            <Input type="email" />: null}
             {method === "phone" ? (
-              <div>
-                <span>+82</span>
-                <input type="number" required />
-              </div>
+              <Number>
+                <span className="flex items-center justify-center px-3 rounded-l-md border 
+                border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+                  +82
+                </span>
+                <Input type="number" required />
+              </Number>
               ) : null}
           </div>
-          <button>
+          <LoginButton>
             {method === "email" ? "Get login link" : null}
             {method === "phone" ? "Get one-time password" : null}
-          </button>
-        </form>
-        <div>
-        <div>
-          <div>
-            <div />
-            <div>
-              <span>Or enter with</span>
+          </LoginButton>
+        </Form>
+        <div className="mt-8">
+          <div className="relative">
+            <div className="absolute w-full border-t border-gray-300" />
+            <div className="relative -top-3 text-center ">
+              <span className="bg-white px-2 text-sm text-gray-500">
+                Or enter with
+              </span>
             </div>
           </div>
-          <div>
-            <button>
+          <div className="grid grid-cols-2 mt-2 gap-3">
+            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -67,7 +158,7 @@ export default function Home() {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button>
+            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -84,7 +175,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-        </div>
     </Container>
     </>
     
